9d4c5db148cabb45e9d33cad7092dca784bcaea0
Andrew <andreiduffy@gmail.com>
2019-05-13 12:48:15 +0300

    2.6 - reviews-video page begin

---
+++ reviews-video.php
@@ -0,0 +1,444 @@
+<?php include('header.php'); ?>
+
+
+<section class="s-reviews-header">
+	<div class="container reviews-header">
+		<div class="row">
+			<div class="grid-6 grid-8_l grid-12_s">
+				<h1 class="h1 reviews-header__title">Отзывы пациентов</h1>
+				<div class="reviews-header__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.</div>
+				<a href="#review-send" class="button scrollto">Оставить отзыв</a>
+			</div>
+			<div class="grid-6 grid-12_l">
+				<div class="reviews-header-counter">
+					<header>
+						<img src="/img/reviews/youtube-icon.svg" alt="youtube-icon">
+					</header>
+					<strong>765</strong>
+					<small>видео отзыва<br>у&nbsp;нас</small>
+				</div>
+				<div class="reviews-header-counter">
+					<header>
+						<img src="/img/reviews/insta-icon.svg" alt="instagram-icon">
+					</header>
+					<strong>1765</strong>
+					<small>селфи пациентов<br>с&nbsp;врачами</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<section class="s-order">
+	<div class="container">
+		<div class="row">
+			<div class="grid-6 grid-12_m order-text">
+				<div class="h1 order-text__title">Хотите пообщаться с&nbsp;пациентами лично?</div>
+				<div class="order-text__text">Мы понимаем, как это важно – убедиться и&nbsp;услышать разные мнения. И&nbsp;не&nbsp;только со&nbsp;стороны врача, но&nbsp;и&nbsp;со&nbsp;стороны пациента. Мы организуем встречи в&nbsp;клинике для&nbsp;пациентов еженедельно. Вы сможете задать все вопросы об&nbsp;их&nbsp;опыте самостоятельно и&nbsp;пообщаться за&nbsp;чашкой вкусного кофе.</div>
+			</div>
+		</div>
+		<div class="bordered-block bordered-block--pink bordered-block--small order-form-block">
+			<form action="#" class="order-form">
+				<div class="order-form__row">
+					<input type="tel" name="user_tel" placeholder="Введите телефон*" class="input-text">
+					<button type="submit" class="button">записаться онлайн</button>
+				</div>
+				<div class="form-note">Нажимая кнопку «Записаться онлайн», я дая согласие на обработку личных данных и соглашаюсь с политикой конфедициальности</div>
+			</form>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-reviews-list">
+	<div class="container">
+		<div class="h1 sec-title">Похожие отзывы</div>
+
+		<div class="row review-intro-list">
+			<div class="grid-4 grid-6_l grid-12_s review-intro-wrap">
+				<div class="review-intro review-intro--video">
+					<a href="/reviews-video.php" class="review-intro__title">«Трансформация улыбки»: интервью Ольги Пыховой после первой коррекции протеза</a>
+					<a href="/reviews-video.php" class="review-intro__screen">
+						<img src="/img/reviews/video-screen.jpg" alt="1">
+						<button class="play-btn"></button>
+					</a>
+					<footer class="review-intro__footer">
+						<time datetime="24-01-2019" class="review-intro__time">24.01.2019</time>
+						<div class="review-intro__type review-intro__type--video">Видеоотзыв</div>
+						<a href="/reviews-video.php" class="review-intro__more">Стоимость и сроки</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_l grid-12_s review-intro-wrap">
+				<div class="review-intro review-intro--video">
+					<a href="/reviews-video.php" class="review-intro__title">«Трансформация улыбки»: интервью Ольги Пыховой после первой коррекции протеза</a>
+					<a href="/reviews-video.php" class="review-intro__screen">
+						<img src="/img/reviews/video-screen.jpg" alt="1">
+						<button class="play-btn"></button>
+					</a>
+					<footer class="review-intro__footer">
+						<time datetime="24-01-2019" class="review-intro__time">24.01.2019</time>
+						<div class="review-intro__type review-intro__type--video">Видеоотзыв</div>
+						<a href="/reviews-video.php" class="review-intro__more">Стоимость и сроки</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_l grid-12_s review-intro-wrap">
+				<div class="review-intro review-intro--video">
+					<a href="/reviews-video.php" class="review-intro__title">«Трансформация улыбки»: интервью Ольги Пыховой после первой коррекции протеза</a>
+					<a href="/reviews-video.php" class="review-intro__screen">
+						<img src="/img/reviews/video-screen.jpg" alt="1">
+						<button class="play-btn"></button>
+					</a>
+					<footer class="review-intro__footer">
+						<time datetime="24-01-2019" class="review-intro__time">24.01.2019</time>
+						<div class="review-intro__type review-intro__type--video">Видеоотзыв</div>
+						<a href="/reviews-video.php" class="review-intro__more">Стоимость и сроки</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_l grid-12_s review-intro-wrap">
+				<div class="review-intro review-intro--video">
+					<a href="/reviews-video.php" class="review-intro__title">«Трансформация улыбки»: интервью Ольги Пыховой после первой коррекции протеза</a>
+					<a href="/reviews-video.php" class="review-intro__screen">
+						<img src="/img/reviews/video-screen.jpg" alt="1">
+						<button class="play-btn"></button>
+					</a>
+					<footer class="review-intro__footer">
+						<time datetime="24-01-2019" class="review-intro__time">24.01.2019</time>
+						<div class="review-intro__type review-intro__type--video">Видеоотзыв</div>
+						<a href="/reviews-video.php" class="review-intro__more">Стоимость и сроки</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_l grid-12_s review-intro-wrap">
+				<div class="review-intro review-intro--video">
+					<a href="/reviews-video.php" class="review-intro__title">«Трансформация улыбки»: интервью Ольги Пыховой после первой коррекции протеза</a>
+					<a href="/reviews-video.php" class="review-intro__screen">
+						<img src="/img/reviews/video-screen.jpg" alt="1">
+						<button class="play-btn"></button>
+					</a>
+					<footer class="review-intro__footer">
+						<time datetime="24-01-2019" class="review-intro__time">24.01.2019</time>
+						<div class="review-intro__type review-intro__type--video">Видеоотзыв</div>
+						<a href="/reviews-video.php" class="review-intro__more">Стоимость и сроки</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_l grid-12_s review-intro-wrap">
+				<div class="review-intro review-intro--video">
+					<a href="/reviews-video.php" class="review-intro__title">«Трансформация улыбки»: интервью Ольги Пыховой после первой коррекции протеза</a>
+					<a href="/reviews-video.php" class="review-intro__screen">
+						<img src="/img/reviews/video-screen.jpg" alt="1">
+						<button class="play-btn"></button>
+					</a>
+					<footer class="review-intro__footer">
+						<time datetime="24-01-2019" class="review-intro__time">24.01.2019</time>
+						<div class="review-intro__type review-intro__type--video">Видеоотзыв</div>
+						<a href="/reviews-video.php" class="review-intro__more">Стоимость и сроки</a>
+					</footer>
+				</div>
+			</div>
+		</div>
+
+		<div class="pagination">
+			<a href="/reviews.php" class="big-gray-btn">Все отзывы (765)</a>
+		</div>
+
+	</div>
+</section>
+
+
+<section class="s-facts s-facts--reviews s-def">
+	<div class="container">
+		<div class="facts-panels row">
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-1.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">3D диагностика и планирование операций</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-2.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Новые зубы буквально сразу после имплантации</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/pop-service-4.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Отличная эстетика улыбки</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-4.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Без синус-лифтингаи наращивание костной ткани</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-5.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Безошибочное хирургическое вмешательство</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-6.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Применяем лучшие бренды имплантов и материалов</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-7.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Эффективная борьба против осложнений</div>
+				</a>
+			</div>
+			<div class="grid-3 grid-6_m fact-panel-wrap">
+				<a href="#" class="fact-panel">
+					<header>
+						<img src="" data-lazy="/img/home/fact-8.svg" alt="1">
+					</header>
+					<div class="facts-panel__title">Забота после и пожизненная гарантия на работу</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-prizes s-def">
+	<div class="container">
+		<div class="h1 sec-title">Наши партнеры и&nbsp;награды</div>
+	</div>
+
+	<div class="prizes-slider swiper-container">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/1.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/1_1.png" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Статус лучшая стоматология страны 2018</div>
+					<div class="prize-block__descr">Медаль лучшие стоматологии РФ 2018</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/2.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/2_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Права потребителей и качество обслуживания</div>
+					<div class="prize-block__descr">Награда Права потребителей и качество обслуживания</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/3.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/3_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">ТОП-100 лучших стоматологий России</div>
+					<div class="prize-block__descr">Сертификат STARTSMILE</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/4.jpg" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/4_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Инновационный центр СтАР</div>
+					<div class="prize-block__descr">Диплом Стоматологической Ассоциации России</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/5.jpg" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/5_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Международная премия «Грация»</div>
+					<div class="prize-block__descr">«Лучшая стоматологическая клиника. Доверие и репутация» «Лучшая сеть стоматологических клиник»</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/6.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/6_1.png" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Премия «Время инноваций»</div>
+					<div class="prize-block__descr">«Лучшая стоматологическая клиника. Доверие и репутация» «Лучшая сеть стоматологических клиник»</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/7.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/7_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">ТОП-100 лучших предприятий и организаций страны</div>
+					<div class="prize-block__descr">Награда 100 лучших предприятий и организаций</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/8.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/8_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">100 лучших медицинских организаций</div>
+					<div class="prize-block__descr">Награда 100 лучших медицинских организаций</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/9.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/9_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">ISO ООО "Стоматология новая улыбка"</div>
+					<div class="prize-block__descr">Сертификат соответствия</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/10.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/10_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Высокое качество "Стоматология новая улыбка"</div>
+					<div class="prize-block__descr">Сертификат соответствия</div>
+				</a>
+			</div>
+			<div class="swiper-slide">
+				<a href="#" class="prize-block">
+					<header class="prize-block__header">
+						<div class="slide-loading"></div>
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/11.png" alt="1" class="swiper-lazy">
+						<img src="/img/prizes/placeholder.png" data-src="/img/prizes/11_1.jpg" alt="1" class="swiper-lazy">
+					</header>
+					<div class="prize-block__name">Инновация года ООО "Стоматология новая улыбка"</div>
+					<div class="prize-block__descr">Сертификат соответствия</div>
+				</a>
+			</div>
+		</div>
+		<div class="slider-arrow slider-arrow--prev">
+			<span class="i i-left"></span>
+		</div>
+		<div class="slider-arrow slider-arrow--next">
+			<span class="i i-right"></span>
+		</div>
+	</div>
+
+</section>
+
+
+<section class="s-def" id="review-send">
+	<div class="container">
+		<div class="h1 sec-title">Вы наш клиент? — Поделитесь своими впечатлениями</div>
+		<div class="bordered-block bordered-block--pink">
+			<form action="#" class="review-form">
+				<div class="h2 review-form__title">Поставьте свою оценку клинике по следующим критериям</div>
+				<div class="row review-form__ratings">
+					<div class="grid-4 grid-6_s grid-12_xs">
+						<!-- Рейтинг из звезд записываетсся в соседний скрытый инпут-->
+						<div class="stars">
+							<div class="stars__title">Компетентность врачей</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_1">
+						</div>
+						<div class="stars">
+							<div class="stars__title">Оборудование</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_2">
+						</div>
+						<div class="stars">
+							<div class="stars__title">Чистота</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_3">
+						</div>
+					</div>
+					<div class="grid-4 grid-6_s grid-12_xs">
+						<div class="stars">
+							<div class="stars__title">Сервис</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_4">
+						</div>
+						<div class="stars">
+							<div class="stars__title">Интерьер</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_5">
+						</div>
+						<div class="stars">
+							<div class="stars__title">Результат лечения</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_6">
+						</div>
+					</div>
+					<div class="grid-4 grid-6_s grid-12_xs">
+						<div class="stars">
+							<div class="stars__title">Цена/качество</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_7">
+						</div>
+						<div class="stars">
+							<div class="stars__title">Будете нас рекомендовать?</div>
+							<div class="stars-rating"></div>
+							<input type="hidden" name="rating_8">
+						</div>
+					</div>
+				</div>
+				<div class="h3 review-form__title">Дополните отзыв своими словами</div>
+				<textarea name="user_review" placeholder="Ваш отзыв" class="input-text"></textarea>
+				<div class="row">
+					<div class="grid-4 grid-12_m">
+						<input type="text" name="user_name" placeholder="Введите имя" class="input-text">
+					</div>
+					<div class="grid-4 grid-12_m">
+						<input type="tel" name="user_tel" placeholder="Введите телефон*" class="input-text danger">
+					</div>
+					<div class="grid-4 grid-12_m">
+						<input type="email" name="user_email" placeholder="Введите e-mail" class="input-text">
+					</div>
+				</div>
+				<div class="review-form__footer">
+					<span>Нажимая кнопку «Отправить», я даю согласие на обработку персональных данных и соглашаюсь с условиями политики конфиденциальности</span>
+					<button type="submit" class="button">Отправить</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
