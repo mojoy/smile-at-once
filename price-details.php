66c01039b8e7ba21ba2b51f9e6163527344aa046
Andrew <andreiduffy@gmail.com>
2019-08-08 12:19:53 +0300

    3.91 - price page details

---
+++ price-details.php
@@ -0,0 +1,340 @@
+<?php include('header.php'); ?>
+
+<section class="s-price">
+	<div class="container">
+
+<!--main-->
+<main class="s-price__main">
+
+	<div class="price-details-header">
+		<h1 class="h1 price-details-header__title">Что входит в стоимость лечения</h1>
+		<div class="price-details-header__shout h3">Полный перечень манипуляций для комплексных решений имплантации и протезирования зубов All-on-4, All-on-6, Trefoil и «Базальный комплекс»</div>
+		<div class="user-content price-details-header__descr">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+		</div>
+	</div>
+
+	<div class="price-details-banner">
+		<div class="garant-banner">
+			<div class="garant-banner__content user-content">
+				<div class="h2 garant-banner__title">Высокий сервис и гарантия после лечения</div>
+				<p>Пожизненная гарантия и бесплатное сервисное обслуживание после лечения.</p>
+				<p>Бесплатная ежегодная профилактика и гигиена имплантов и протезов, наблюдение врача, устранение возможных угроз и осложнений. Оберегаем достигнутый результат на профессиональном уровне, чтобы новая улыбка служила долгие годы!</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="price-details-main">
+		<div class="h2 price-details-main__title">Что включено в стоимость</div>
+		<div class="border-drops border-drops--list">
+			<div class="border-drop">
+				<div class="border-drop__header">Компьютерная томография и мультиспиральная компьютерная томография –
+					все снимки «ДО» и «ПОСЛЕ»  имплантации</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-drop">
+				<div class="border-drop__header">Анализы перед операцией</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-drop">
+				<div class="border-drop__header">Местная анастезия, которая подбирается индивидуально для пациента</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-drop">
+				<div class="border-drop__header">Удаление зубов перед имплантацией</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="price-details-main">
+		<div class="h2 price-details-main__title">Что не включено в стоимость</div>
+		<div class="border-drops border-drops--list">
+			<div class="border-drop">
+				<div class="border-drop__header">Седация и общий наркоз</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-drop">
+				<div class="border-drop__header">Лечение и/или альтернативные варианты восстановления зубов-антагонистов</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-drop">
+				<div class="border-drop__header">Костная пластика</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-drop">
+				<div class="border-drop__header">Комплексная гигиена полости рта перед имплантацией</div>
+				<div class="border-drop__content">
+					<div class="price-include row">
+						<div class="price-include__main grid-8 grid-12_m">
+							<div class="user-content">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+							</div>
+						</div>
+						<div class="price-include__aside grid-4 grid-12_m">
+							<strong class="price-include__price">от 3 500 руб. за 1 челюсть</strong>
+							<strong class="price-include__price">от 5 000 руб. за 2 челюсти</strong>
+							<div class="price-include__note">У нас стоимость КТ входит даже в бесплатную консультацию – вы ничего не платите.</div>
+						</div>
+						<div class="grid-12">
+							<div class="price-item__links">
+								<a href="#modal-callback" class="link-arrow fancy-modal">Остались вопросы? <span class="i i-ar-right"></span></a>
+								<a href="#" class="link-arrow">Подробнее о манипуляциях <span class="i i-ar-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="price-details-promo">
+		<a href="#" class="promo-banner-mini promo-banner-mini--yellow">
+		<div class="promo-banner-mini__imgwrap">
+			<img src="/img/promo/placeholder.png" data-lazy="/img/price/promo-1.png" alt="1" class="promo-banner-mini__img">
+		</div>
+		<div class="promo-banner-mini__content">
+			<div class="promo-banner-mini__title">Протезирование на&nbsp;имплантах Trefoil для&nbsp;нижней челюсти</div>
+			<div class="promo-banner-mini__prices">
+				<div class="promo-banner-mini__price">320 000 руб</div>
+				<div class="promo-banner-mini__oldprice">350 000 руб</div>
+			</div>
+		</div>
+		<div class="promo-banner-mini__date">до 18.03.2019</div>
+		<span class="link-arrow promo-banner-mini__link">Подробнее <span class="i i-ar-right"></span></span>
+	</a>
+	</div>
+
+	<div class="price-details-after-sec">
+		<div class="h2 price-details-main__title">Что вы получаете после проведения имплантации</div>
+		<div class="row-ng">
+			<div class="grid-6 grid-12_m">
+				<div class="price-details-after bordered-block bordered-block--small">
+					<div class="price-details-after__icon">
+						<img src="/img/price/after-1.png" alt="">
+					</div>
+					<div class="h3">Качественное и быстрое решение вашей проблемы</div>
+					<ul class="checked-green-list">
+						<li>Начните жевать сразу!</li>
+						<li>Наслаждайтесь вкусом еды!</li>
+						<li>Питайтесь любыми продуктами, а не только мягкой пищей!</li>
+						<li>Забудьте о боли и дискомфорте!</li>
+						<li>Протезы больше не нужно снимать, зубы ощущаются как родные!</li>
+						<li>Перестанете стесняться!</li>
+						<li>Начнете улыбаться и радоваться жизни!</li>
+					</ul>
+				</div>
+			</div>
+			<div class="grid-6 grid-12_m">
+				<div class="price-details-after bordered-block bordered-block--small">
+					<div class="price-details-after__icon">
+						<img src="/img/price/after-2.png" alt="">
+					</div>
+					<div class="h3">Оптимальная стоимость за комплексный объем работ</div>
+					<ul class="checked-green-list">
+						<li>Конечная стоимость восстановления зубов с немедленной нагрузкой всегда ниже аналогичного варианта восстановления зубов по двухэтапному протоколу с отсроченной нагрузкой.</li>
+						<li>Благодаря владению всеми методами протокола мы, как правило, можем предложить несколько вариантов на выбор.</li>
+						<li>При необходимости, мы поможем оформить кредит и рассрочку.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="price-footer">
+		<div class="price-seo user-content">
+			<div class="h1">Seo текст</div>
+			<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.</div>
+		</div>
+	</div>
+
+</main>
+<!--main-->
+
+<!--aside-->
+<aside class="s-price__aside">
+	<div class="aside-sticky">
+		<div class="aside-triggers">
+			<a href="#modal-callback" class="pop-up fancy-modal aside-triggers__img" title="Бесплатная консультация нескольких врачей">
+				<img src="/img/price/aside-doctors.jpg" title="Бесплатная консультация нескольких врачей" alt="Бесплатная консультация нескольких врачей">
+			</a>
+			<div class="aside-triggers__top">
+				<strong>
+					Диагностика и консультация
+					<span>Бесплатно!</span>
+				</strong>
+				<em>Включая КТ и ОПТГ</em>
+				<a href="#modal-callback" class="pop-up fancy-modal button button--small" title="ЗАПИСАТЬСЯ">ЗАПИСАТЬСЯ</a>
+			</div>
+			<a href="/regions.html" title="Дистанционная консультация" class="aside-triggers-item">
+				<span class="aside-triggers-item__img">
+					<img src="/img/price/aside-ic-1.png" alt="1">
+				</span>
+				<div>
+					<strong>Дистанционная консультация</strong>
+					для пациентов из других городов
+				</div>
+			</a>
+			<a href="/otzivi_o_stomatologii/" title="Видео до и после лечения" class="aside-triggers-item">
+				<span class="aside-triggers-item__img">
+					<img src="/img/price/aside-ic-2.png" alt="2">
+				</span>
+				<div>
+					<strong>Видео до и после лечения</strong>
+					Более 900 отзывов о нашей работе
+				</div>
+			</a>
+			<a href="/nagrady-i-dostizheniya.html" title="Федеральные награды" class="aside-triggers-item">
+				<span class="aside-triggers-item__img">
+					<img src="/img/price/aside-ic-3.png" alt="3">
+				</span>
+				<div>
+					<strong>Федеральные награды</strong>
+					12 наград и премий за успехи
+				</div>
+			</a>
+		</div>
+	</div>
+</aside>
+<!--aside-->
+
+	</div>
+</section>
+
+
+
+<?php include('footer.php'); ?>
--- price.php
+++ price.php
@@ -1937,7 +1937,7 @@
 				<img src="/img/price/pay-3.png" alt="">
 			</div>
 		</div>
-		<div class="price-seo">
+		<div class="price-seo user-content">
 			<div class="h1">Seo текст</div>
 			<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.</div>
 		</div>
