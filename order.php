a752c29ebeea653f8173183c473773edeb95587b
alex <alex.shamarin@gmail.com>
2020-06-15 22:48:02 +0300

    cabinet order

---
+++ order.php
@@ -0,0 +1,491 @@
+<?php include('header.php'); ?>
+
+
+
+<section class="order-view">
+	<div class="container">
+		<h1 class="h1 sec-title">ЗАКАЗ №083 от <span class="order-date">14.06.2020</span></h1>
+
+
+		<h2 class="order-sub-title">Статус: <span class="order-status">ОТПРАВЛЕН - <span class="order-status-text order-status--urgent">СРОЧНЫЙ</span></span></h2>
+		<div class="row order-info-patient">
+			<div class="grid-6 grid-12_l">
+				<table class="n-table">
+					<tr>
+						<td class="n-table__th order-info-patient__td">ФИО пациента:</td>
+						<td class="n-table__td order-info-patient__td">Титов Михаил Валерьевич</td>
+					</tr>
+					<tr>
+						<td class="n-table__th order-info-patient__td">Дата операции:</td>
+						<td class="n-table__td order-info-patient__td">20.06.2020</td>
+					</tr>
+					<tr>
+						<td class="n-table__th order-info-patient__td">Имплантолог (ответственный врач):</td>
+						<td class="n-table__td order-info-patient__td">Копылов Иван Павлович</td>
+					</tr>
+					<tr>
+						<td class="n-table__th order-info-patient__td">Ортопед (Врач 2): </td>
+						<td class="n-table__td order-info-patient__td">Чернов Андрей Ростиславович</td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="grid-6 grid-12_l">
+				<table class="n-table">
+					<tr>
+						<td class="n-table__th order-info-patient__td">Вид работы: </td>
+						<td class="n-table__td order-info-patient__td">All-on-4</td>
+					</tr>
+					<tr>
+						<td class="n-table__th order-info-patient__td">Вид нагрузки:</td>
+						<td class="n-table__td order-info-patient__td">Немедленная</td>
+					</tr>
+					<tr>
+						<td class="n-table__th order-info-patient__td">Согласованный план: </td>
+						<td class="n-table__td order-info-patient__td"><span class="meeting-register__status meeting-register__status-completed" title="Согласованный">Согласованный</span></td>
+					</tr>
+					<tr>
+						<td class="n-table__th order-info-patient__td">Согласованный план: </td>
+						<td class="n-table__td order-info-patient__td"><span class="meeting-register__status meeting-register__status-completed" title="Согласованный">Согласованный</span></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+
+		<div class="order-info">
+			<h3>История заказа:</h3>
+			<table class="order-info-list">
+				<tr>
+					<th class="order-info-list__title">Отправил:</th>
+					<td class="order-info-list__text">Чернов Андрей Растиславович,</td>
+					<td class="order-info-list__date">18.06.2020</td>
+				</tr>
+				<tr>
+					<th class="order-info-list__title">Создал:</th>
+					<td class="order-info-list__text">Копылов Иван Павлович, </td>
+					<td class="order-info-list__date">14.06.2020</td>
+				</tr>
+				<tr>
+					<th class="order-info-list__title" colspan="2">Дата последнего изменения:</th>
+					<td class="order-info-list__date">19.06.2020</td>
+				</tr>
+			</table>
+		</div>
+
+		<h2 class="h2 sec-title">Схема расстановки имплантов</h2>
+
+		<table class="order-table-schema">
+			<tr>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+			</tr>
+			<tr>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+			</tr>
+			<tr>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">18</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">17</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">16</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">15</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">14</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">13</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">12</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">11</td>
+
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">21</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">22</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">23</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">24</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">25</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">26</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">27</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">28</td>
+			</tr>
+			<tr>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">48</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">47</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">46</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">45</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">44</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">43</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">42</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">41</td>
+
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">31</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">32</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">33</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">34</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">35</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">36</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">37</td>
+				<td class="order-table-schema__td order-table-schema__zub" title="номер зуба">38</td>
+			</tr>
+			<tr>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__size" title="размер">4,3х13мм</span>
+					<span class="order-table-schema__size" title="размер">4,3х15мм</span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+			</tr>
+			<tr>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td">
+					<span class="order-table-schema__angle" title="протетика (градус)">17<span>&deg;</span></span>
+					<span class="order-table-schema__angle" title="протетика (градус)">30<span>&deg;</span></span>
+				</td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+				<td class="order-table-schema__td"></td>
+			</tr>
+		</table>
+
+		<h2 class="h2 sec-title">Параметры заказа</h2>
+
+		<h3 class="h3">Импланты</h3>
+
+		<table class="n-table">
+			<thead>
+				<tr>
+					<th class="n-table__th n-table__number">№ зуба</th>
+					<th class="n-table__th">Признак</th>
+					<th class="n-table__th">Наименование</th>
+					<th class="n-table__th n-table__number">Размер</th>
+					<th class="n-table__th n-table__number">Артикул</th>
+					<th class="n-table__th n-table__number">Количество</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td class="n-table__td n-table__number">17</td>
+					<td class="n-table__td">Основной</td>
+					<td class="n-table__td">Nobel Biocare - Parallel CC RP</td>
+					<td class="n-table__td">4.3x10mm</td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">17</td>
+					<td class="n-table__td">Резерв</td>
+					<td class="n-table__td">Nobel Biocare - Parallel CC RP</td>
+					<td class="n-table__td">4.3x15mm</td>
+					<td class="n-table__td n-table__number">37975</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">28</td>
+					<td class="n-table__td">Основной</td>
+					<td class="n-table__td">Nobel Biocare - Parallel CC RP</td>
+					<td class="n-table__td">4.3x15mm</td>
+					<td class="n-table__td n-table__number">37975</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">38</td>
+					<td class="n-table__td">Основной</td>
+					<td class="n-table__td">Nobel Biocare - Parallel CC RP</td>
+					<td class="n-table__td">4.3x15mm</td>
+					<td class="n-table__td n-table__number">37975</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">38</td>
+					<td class="n-table__td">Резерв</td>
+					<td class="n-table__td">Nobel Biocare - Parallel CC RP</td>
+					<td class="n-table__td">4.3x15mm</td>
+					<td class="n-table__td n-table__number">37975</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td class="n-table__td" colspan="5">ИТОГО:</td>
+					<td class="n-table__td n-table__number">5</td>
+				</tr>
+			</tfoot>
+
+		</table>
+
+
+
+
+		<h3 class="h3">Протетика</h3>
+
+		<table class="n-table">
+			<thead>
+				<tr>
+					<th class="n-table__th n-table__number">№ зуба</th>
+					<th class="n-table__th">Признак</th>
+					<th class="n-table__th">Вид протетики</th>
+					<th class="n-table__th">Наименование</th>
+					<th class="n-table__th n-table__number">Артикул</th>
+					<th class="n-table__th n-table__number">Количество</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td class="n-table__td n-table__number">17</td>
+					<td class="n-table__td">Основной</td>
+					<td class="n-table__td">Абатмент</td>
+					<td class="n-table__td">Абатмент MULTI-UNIT BMK RP 2MM </td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">17</td>
+					<td class="n-table__td">Резерв</td>
+					<td class="n-table__td">Абатмент</td>
+					<td class="n-table__td">Абатмент MULTI-UNIT BMK RP 2MM </td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">28</td>
+					<td class="n-table__td">Основной</td>
+					<td class="n-table__td">Абатмент</td>
+					<td class="n-table__td">Bmk Syst Zygoma 17° Multi-unit Abut 2mm</td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">28</td>
+					<td class="n-table__td">Резерв</td>
+					<td class="n-table__td">Абатмент</td>
+					<td class="n-table__td">Bmk Syst Zygoma 17° Multi-unit Abut 2mm</td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">38</td>
+					<td class="n-table__td">Основной</td>
+					<td class="n-table__td">Абатмент</td>
+					<td class="n-table__td">Bmk Syst Zygoma 17° Multi-unit Abut 2mm</td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+				<tr>
+					<td class="n-table__td n-table__number">38</td>
+					<td class="n-table__td">Резерв</td>
+					<td class="n-table__td">Абатмент</td>
+					<td class="n-table__td">Bmk Syst Zygoma 17° Multi-unit Abut 2mm</td>
+					<td class="n-table__td n-table__number">37972</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+
+			</tbody>
+			<tfoot>
+				<tr>
+					<td class="n-table__td" colspan="5">ИТОГО:</td>
+					<td class="n-table__td n-table__number">6</td>
+				</tr>
+			</tfoot>
+
+		</table>
+
+
+
+		<h3 class="h3">Инструменты</h3>
+
+		<table class="n-table">
+			<thead>
+				<tr>
+					<th class="n-table__th">Наименование</th>
+					<th class="n-table__th n-table__number">Артикул</th>
+					<th class="n-table__th n-table__number">Количество</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td class="n-table__td">Ортопедический набор </td>
+					<td class="n-table__td n-table__number">37448</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+				<tr>
+					<td class="n-table__td">Установочный инструмент для GCS A 15°, длинный, SS 7300</td>
+					<td class="n-table__td n-table__number">BM1341</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td class="n-table__td" colspan="2">ИТОГО:</td>
+					<td class="n-table__td n-table__number">2</td>
+				</tr>
+			</tfoot>
+		</table>
+
+
+
+		<h3 class="h3">Биоматериал</h3>
+
+		<table class="n-table">
+			<thead>
+				<tr>
+					<th class="n-table__th">Наименование</th>
+					<th class="n-table__th n-table__number">Количество</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td class="n-table__td">Коллагеновая мембрана Ossix plus 30*40 мм</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+				<tr>
+					<td class="n-table__td">Коллагеновая мембрана Ossix plus 30*40 мм</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+				<tr>
+					<td class="n-table__td">Bio-Oss 2 г, гранулы 0,25-1 мм, размер S, натуральный костнозамещающий материал.</td>
+					<td class="n-table__td n-table__number">1</td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td class="n-table__td">ИТОГО:</td>
+					<td class="n-table__td n-table__number">3</td>
+				</tr>
+			</tfoot>
+		</table>
+
+
+
+		<h3 class="h3">Другое</h3>
+
+		<table class="n-table">
+			<thead>
+			<tr>
+				<th class="n-table__th">Наименование</th>
+				<th class="n-table__th n-table__number">Количество</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td class="n-table__td">Текст свободного содержания</td>
+				<td class="n-table__td n-table__number">1</td>
+			</tr>
+			<tr>
+				<td class="n-table__td">Текст свободного содержания</td>
+				<td class="n-table__td n-table__number">1</td>
+			</tr>
+			</tbody>
+			<tfoot>
+			<tr>
+				<td class="n-table__td">ИТОГО:</td>
+				<td class="n-table__td n-table__number">2</td>
+			</tr>
+			</tfoot>
+		</table>
+
+
+
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
